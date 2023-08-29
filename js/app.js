17dcf428a4c74816fd63c7846683eaa1f4f40e31
bishara apdirahman <bisharaarahman262@gmail.com>
2023-08-29 18:40:12 +0100

    commited

--- js/app.js
+++
@@ -1,5 +0,0 @@
-function submitData() {
-    var password = document.getElementById('pass').value;
-    console.log('password:', password.trim());
-  }
-  
--- js/nav.js
+++
@@ -1,6 +0,0 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarlinks = document.getElementsByClassName('center_part')[0]
-
-toggleButton.addEventListener('click', () =>{
-    navbarlinks.classList.toggle('below')
-})
